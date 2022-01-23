91435d55fb4e66c47cd8669807bf54c18a563ceb
Sümeyye Üzgör <sumeyye.uzgor@venturelabs.team>
2022-01-23 12:49:34 +0300

    task details on task card

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -8,18 +8,18 @@
   >
     <v-card-title>{{ task.title }}</v-card-title>
     <v-list>
-      <v-list-item>
-        {{ task.title }}
+      <v-list-item class="indigo--text darken-3">
+        Assigned To: {{ getAssignedDepartment }}
       </v-list-item>
-      <v-list-item>
-        {{ task.title }}
+      <v-list-item class="indigo--text darken-3">
+        Status: {{ getStatus }}
       </v-list-item>
     </v-list>
     <v-card-actions>
-      <v-btn outlined>deneme</v-btn>
-      <v-btn outlined>deneme</v-btn>
-      <v-btn outlined>deneme</v-btn>
-      <v-btn outlined>deneme</v-btn>
+      <v-btn outlined class="indigo--text darken-3">Accept</v-btn>
+      <v-btn outlined class="indigo--text darken-3">Reject</v-btn>
+      <v-btn outlined class="indigo--text darken-3">Complete</v-btn>
+      <v-btn outlined class="indigo--text darken-3">Delete</v-btn>
     </v-card-actions>
   </v-card>
 </template>
@@ -32,8 +32,42 @@ export default {
       default: () => ({
         title: "",
         id: "",
-        assignement: "",
+        assignedDepartment: 0,
+        status: 0,
       }),
+    },
+  },
+  computed: {
+    getAssignedDepartment() {
+      let department = "";
+      switch (this.task.assignedDepartment) {
+        case 1:
+          department = "Human Resources";
+          break;
+        case 2:
+          department = "Sales and Marketing";
+          break;
+        default:
+          break;
+      }
+      return department;
+    },
+    getStatus() {
+      let status = "";
+      switch (this.task.status) {
+        case 0:
+          status = "Pending";
+          break;
+        case 1:
+          status = "In Progress";
+          break;
+        case 2:
+          status = "Completed";
+          break;
+        default:
+          break;
+      }
+      return status;
     },
   },
 };
